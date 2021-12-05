464fc10ec1b44eff51dc91ad4f3c4ed3a9fe8e2e
Marika-G <marika_groleau@hotmail.com>
2021-12-05 15:33:01 -0500

    Day1 part 2

--- 1.rb
+++ 1.rb
@@ -4,6 +4,7 @@ class Depth_o_meter
     def initialize(measures_file)
         @raw_depth_measures = IO.readlines(measures_file).collect{|i| i.to_i}
         @depth_diff = calc_diff(@raw_depth_measures)
+        @depth_diff_window = calc_diff_window(@raw_depth_measures)
     end
     def calc_diff(measures)
         diff = []
@@ -12,12 +13,27 @@ class Depth_o_meter
         end
         return diff
     end
+    def calc_diff_window(measures)
+        measures_summed_window = []
+        for i in (0..measures.length-3)
+            measures_summed_window.push(measures[i...i+3].sum)
+        end
+        return calc_diff(measures_summed_window)
+    end
     def get_increases
+        return calc_increases(@depth_diff)
+    end
+    def get_increases_window
+        return calc_increases(@depth_diff_window)
+    end
+    protected
+    def calc_increases(measures)
         nb = 0
-        @depth_diff.each{|i| nb += 1 if i > 0}
+        measures.each{|i| nb += 1 if i > 0}
         return nb
     end
 end
 
 meter = Depth_o_meter.new(ARGV[0])
 puts meter.get_increases
+puts meter.get_increases_window
