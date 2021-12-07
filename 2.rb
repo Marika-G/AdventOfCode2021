9ecb784f4b0c85e4062466ec0f7c52bbca52edc8
Jean-Samuel Aubry-Guzzi <DrPandemic@users.noreply.github.com>
2021-12-06 22:00:37 -0500

    Update 2.rb

--- 2.rb
+++ 2.rb
@@ -6,15 +6,15 @@ class Submarine
         @position_y = 0
         @aim = 0
     end
-    def get_position
-        return @position_x,@position_y
-    end
+    
     def apply_instructions(instruction_file)
-        resulting_shifts = read_instructions(instruction_file)
-        move(resulting_shifts[0],resulting_shifts[1])
-        return get_position
+        x, y = read_instructions(instruction_file)
+        move(x, y)
+        @position_x, @position_y
     end
+    
     protected
+    
     def read_instructions(file)
         x = 0
         y = 0
@@ -27,17 +27,18 @@ class Submarine
                 @aim += value.to_i
             when "forward"
                 x += value.to_i
-                y += value.to_i*@aim
+                y += value.to_i * @aim
             end
         end
-        return x,y
+        x, y
     end
-    def move(x,y)
+    
+    def move(x, y)
         @position_x += x
         @position_y += y 
     end
 end
 
 my_submarine = Submarine.new()
-x,y = my_submarine.apply_instructions(ARGV[0])
+x, y = my_submarine.apply_instructions(ARGV[0])
 puts x*y
